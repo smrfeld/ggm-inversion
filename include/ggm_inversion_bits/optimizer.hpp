fd24d9d4e41cf116ee586efb57db1acf8f133ea0
Oliver K. Ernst <oernst@ucsd.edu>
2020-12-04 12:17:31 -0800

    added l bfgs optimizer

--- include/ggm_inversion_bits/optimizer.hpp
+++ include/ggm_inversion_bits/optimizer.hpp
@@ -41,6 +41,14 @@ private:
     std::vector<std::pair<int,int>> _idx_pairs_free;
     int _n_rows, _n_cols;
     
+    double _get_first_deriv_inverse_mat(const arma::mat &cov_mat_curr, int d1, int d2, int n1, int n2) const;
+    double _get_second_deriv_inverse_mat(const arma::mat &cov_mat_curr, int d1, int d2, int d3, int d4, int n1, int n2) const;
+
+    arma::mat _vec_to_mat(const arma::vec &vec) const;
+    arma::vec _mat_to_vec(const arma::mat &mat) const;
+
+    double _get_step_size_armijo_backtrack(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true, const arma::vec &updates, const arma::vec &grads, double c=1e-4) const;
+    
     /// Internal clean up
     void _clean_up();
     /// Internal copy
@@ -59,10 +67,14 @@ public:
 
     double get_obj_func_val(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true) const;
     
-    arma::mat get_derivs(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true) const;
+    arma::mat get_deriv_mat(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true) const;
+    arma::vec get_deriv_vec(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true) const;
+
+    arma::mat get_hessian(const arma::mat &cov_mat_curr, const arma::mat &cov_mat_true) const;
     
     arma::mat solve_sgd(const arma::mat &cov_mat_true, const arma::mat &prec_mat_init, double lr, int no_opt_steps) const;
     arma::mat solve_adam(const arma::mat &cov_mat_true, const arma::mat &prec_mat_init, double lr, int no_opt_steps, double adam_beta_1=0.9, double adam_beta_2=0.999, double adam_eps=1e-8) const;
+    arma::mat solve_l_bfgs(const arma::mat &cov_mat_true, const arma::mat &prec_mat_init, int no_opt_steps, int m, double tol) const;
 };
 
 }
---
+++ test/src/test_5d_l_bfgs.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <ggm_inversion>
+
+#include "spdlog/spdlog.h"
+#include <exception>
+#include <armadillo>
+
+#include "common.hpp"
+
+using namespace std;
+using namespace ggm;
+
+int main() {
+    std::vector<std::pair<int,int>> idx_pairs_free;
+    idx_pairs_free.push_back(std::make_pair(0, 0));
+    idx_pairs_free.push_back(std::make_pair(1, 1));
+    idx_pairs_free.push_back(std::make_pair(2, 2));
+    idx_pairs_free.push_back(std::make_pair(3, 3));
+    idx_pairs_free.push_back(std::make_pair(4, 4));
+
+    idx_pairs_free.push_back(std::make_pair(0, 3));
+    idx_pairs_free.push_back(std::make_pair(1, 2));
+    idx_pairs_free.push_back(std::make_pair(2, 4));
+    idx_pairs_free.push_back(std::make_pair(3, 4));
+
+    arma::mat cov_mat_true = {
+        {100, 0, 0, 20, 0},
+        {0, 80, 3, 0, 0},
+        {0, 3, 6, 0, 4},
+        {20, 0, 0, 40, 10},
+        {0, 0, 4, 10, 60}
+    };
+    
+    int n_rows = 5;
+    int n_cols = 5;
+    Optimizer opt(n_rows, n_cols, idx_pairs_free);
+    
+    arma::mat prec_mat_init = 0.01 * arma::eye(n_rows, n_cols);
+    int m = 10;
+    double tol = 1e-8;
+    arma::mat prec_mat_solved = opt.solve_l_bfgs(cov_mat_true, prec_mat_init, 1000, m, tol);
+
+    report_results(prec_mat_solved, cov_mat_true, idx_pairs_free, opt);
+
+    return 0;
+}
